2c0cda95d9ea8d3647fc3f7ff55d7f9330a07ba6
Jared <jared@interlacedpixel.com>
2022-09-27 17:11:47 -0500

    CLI Init
    Command line interface version of MapleSeed

---
+++ MapleSeed-macOS-CLI/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MapleSeed-macOS-CLI
+//
+//  Created by Interlaced Pixel on 9/27/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
